150151d5d3288139c1797b390cda31e0099bdba2
Vaibhav <vaibhavb2106@gmail.com>
2026-08-20 01:13:55 +0530

    add day 1 question 1 solution

---
+++ day1_code3.c
@@ -0,0 +1,10 @@
+//Q1: Write a program to input two numbers and display their sum.
+#include <stdio.h>
+int main()
+{int n,m,r;
+printf("enter any two numbers= ");
+scanf("%d%d",&n,&m);
+r=n+m;
+printf("sum= %d\n",r);
+return 0;}
+
