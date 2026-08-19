a8fcbce84ec2cea1102605cd6523064d15922e9b
Vaibhav <vaibhavb2106@gmail.com>
2026-08-20 01:22:40 +0530

    add day 3 question 1 solution

---
+++ day3_code1.c
@@ -0,0 +1,9 @@
+//Q5: Write a program to convert temperature from Celsius to Fahrenheit.
+#include <stdio.h>
+float main()
+{float c,f;
+printf("enter the temperature in celcius= ");
+scanf("%f",&c);
+f=(c*9/5)+32;
+printf("fahnerhiet=%f\n",f);
+return 0;}
