93ef9dcee68ce5923a0f0bbd2ea0e5d434696b2c
Vaibhav <vaibhavb2106@gmail.com>
2026-08-20 01:02:14 +0530

    remove all files

--- day4_code1.c
+++
@@ -1,9 +0,0 @@
-//Q7: Write a program to swap two numbers without using a third variable.
-#include <stdio.h>
-int main()
-{int a,b;
-printf("enter any two numbers= ");
-scanf("%d%d",&a,&b);
-printf("after swap: %d %d\n",b,a);
-return 0;}
-
