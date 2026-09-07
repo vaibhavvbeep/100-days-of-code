e0234eb1bf4da8f917834c466f3f16f1bd76f88e
vaibhavvbeep <vaibhavb2106@gmail.com>
2026-09-07 23:27:25 +0530

    day 18 question 1 solution

---
+++ day18_code1.c
@@ -0,0 +1,18 @@
+//Q35: Write a program to print all factors of a given number.
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
